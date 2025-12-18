ab9f121a8540056301c73a4b08e90b4fcfcb406d
Paulo Penalva <paulo.penalva@gmail.com>
2025-12-17 22:49:16 -0300

    commit

---
+++ SiagroB1.Web/Functions/ShipmentReleases/ShipmentReleasesGetBalanceController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+using SiagroB1.Application.Dtos;
+using SiagroB1.Application.ShipmentReleases;
+
+namespace SiagroB1.Web.Functions.ShipmentReleases;
+
+public class ShipmentReleasesGetBalanceController(ShipmentReleasesBalanceService service)
+    : ODataController
+{
+    [EnableQuery]
+    [HttpGet("odata/ShipmentReleasesGetBalance(ItemCode={itemCode})")]
+    public async Task<ActionResult<ICollection<ShipmentRelesesBalanceResponseDto>>> GetAsync([FromRoute] string itemCode)
+    {
+        return Ok(await service.ExecuteAsync(itemCode));
+    }
+}
