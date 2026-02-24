5cef8eb681b6489ba4acc8cd498cfdb6d69163d2
Paulo Penalva <paulo.penalva@gmail.com>
2026-02-24 14:27:54 -0300

    commit

---
+++ SiagroB1.Migrations/AppContext/20260221134344_AlterTablePurchaseContractsAddColumnTechnologyType.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SiagroB1.Migrations.AppContext
+{
+    /// <inheritdoc />
+    public partial class AlterTablePurchaseContractsAddColumnTechnologyType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TechnologyType",
+                table: "PURCHASE_CONTRACTS",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TechnologyType",
+                table: "PURCHASE_CONTRACTS");
+        }
+    }
+}
---
+++ SiagroB1.Migrations/AppContext/20260221182620_AlterTableStorageAddressesAddColumnUoM.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SiagroB1.Migrations.AppContext
+{
+    /// <inheritdoc />
+    public partial class AlterTableStorageAddressesAddColumnUoM : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UoM",
+                table: "STORAGE_ADDRESSES",
+                type: "VARCHAR(4)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UoM",
+                table: "STORAGE_ADDRESSES");
+        }
+    }
+}
